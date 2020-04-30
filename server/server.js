2a8889bf07611d3035667a076ddf0aa321edf7ee
tycompton <tyrone.compton@gmail.com>
2020-04-30 18:25:37 +0100

    server changes for production

--- server/server.js
+++ server/server.js
@@ -10,13 +10,15 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -465,7 +467,13 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
   );
 });
 
-
+// DEFAULT PRODUCTION ROUTE 
+if( process.env.NODE_ENV ? === 'production' ){
+  const path = require('path');
+  app.get('/*',(req,res)=>{
+    res.sendfile(path.resolve(__dirname, '../client','build','index.html'))
+  })
+}
 
 const port = process.env.PORT || 3002;
 app.listen(port,()=>{
