dbdbf314e134e490c0600219d6596c2962c4e417
tycompton <tyrone.compton@gmail.com>
2020-05-11 18:42:32 +0100

    PO function created

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const formidable = require('express-formidable');
 const cloudinary = require('cloudinary');
+const SHA1 = require('crypto-js/sha1');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -38,6 +39,13 @@ const { Site } = require('./models/site');
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
 
+
+
+// const date = new Date();
+// const po = `PO-${date.getSeconds()}${date.getMilliseconds()}=${SHA1("23923729HJG")
+//   .toString().substring(0, 8)}`
+
+//   console.log(po)
 
 // Email config
 
@@ -357,10 +365,17 @@ app.get("/api/users/removeFromCart", auth, (req, res) => {
 app.post('/api/users/success_buy', auth, (req, res) => {
   let history = [];
   let transactionData = {};
+  const date = new Date();
+  const po = `PO-${date.getSeconds()}${date.getMilliseconds()}-${SHA1(
+    "req.user._id"
+  )
+    .toString()
+    .substring(0, 8)}`;
 
   // User History
   req.body.cartDetail.forEach((item) => {
     history.push({
+      porder: po,
       dateOfPurchase: Date.now(),
       name: item.name,
       brand: item.brand.name,
@@ -379,7 +394,10 @@ app.post('/api/users/success_buy', auth, (req, res) => {
     email: req.user.email,
   };
 
-  transactionData.data = req.body.paymentData;
+  transactionData.data = {
+    ...req.body.paymentData,
+    porder: po
+  };
   transactionData.product = history;
 
   User.findOneAndUpdate(
